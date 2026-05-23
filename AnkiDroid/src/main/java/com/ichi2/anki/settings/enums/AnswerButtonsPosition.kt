753891d009e6da161690bdbcba71d9327afbe4a1
Damien Diehl <1700953+damien5314@users.noreply.github.com>
2026-05-22 20:59:44 -0400

    docs

--- AnkiDroid/src/main/java/com/ichi2/anki/settings/enums/AnswerButtonsPosition.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/settings/enums/AnswerButtonsPosition.kt
@@ -1,5 +1,8 @@
 package com.ichi2.anki.settings.enums
 
+/**
+ * Represents possible configurations for the position of the answer buttons on the study screen.
+ */
 enum class AnswerButtonsPosition {
     TOP,
     BOTTOM,
